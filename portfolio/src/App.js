732b364708f2b2fdbb13bf0e65df58808d9b0409
Nethum Imsara Hewaaarachchi <nethumimsara@gmail.com>
2024-07-12 22:44:24 +0530

    create components
    Nav bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Component/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Nethum Imsara Hewaarachchi</h1>
     </div>
   );
